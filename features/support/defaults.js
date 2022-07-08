bccc0ca07ef92364372931dcd4cbc9d6224da764
depapp <depa.article@gmail.com>
2022-07-08 15:29:45 +0700

    :pencil: add more features

--- features/support/defaults.js
+++ features/support/defaults.js
@@ -2,6 +2,5 @@ const { request, settings } = require('pactum');
 const { Before } = require('@cucumber/cucumber');
 
 Before(() => {
-  request.setBaseUrl('https://reqres.in');
   settings.setReporterAutoRun(false);
 });
